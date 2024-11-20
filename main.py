568524a1a7adfb25d489bdd4e076d997fc6cc176
cjwheeler668 <158320425+cjwheeler668@users.noreply.github.com>
2024-11-20 15:35:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print("Password accepted."
 #Menu
 def menu():
   print("*******************************")
-  print("AutoCountry Vehicle Finder v0.1")
+  print("AutoCountry Vehicle Finder v0.4")
   print("*******************************")
   print("Plese enter the following number bellow from the following menu: ")
   print("[1] PRINT all Authorized Vehicles")
